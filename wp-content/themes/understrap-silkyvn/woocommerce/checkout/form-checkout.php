b4f91e0b20e3b8f30a7535a10522da677c04822c
Pham Tuan Duc <89022216+toughguy1010@users.noreply.github.com>
2022-06-23 12:00:24 +0700

    commit -m "12h00 23/6"

--- wp-content/themes/understrap-silkyvn/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/understrap-silkyvn/woocommerce/checkout/form-checkout.php
@@ -18,6 +18,12 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
+	return;
+}
+
+
 do_action('product_style');
 do_action( 'woocommerce_before_checkout_form', $checkout );
 
@@ -35,9 +41,23 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-		<div class="row" id="customer_details">
+		<div class="col" id="customer_details">
 			<div class="col-12 col-sm-7">
 				<?php do_action( 'woocommerce_checkout_billing' ); ?>
+				<form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:block">
+
+					<p><?php esc_html_e( 'If you have a coupon code, please apply it below.', 'woocommerce' ); ?></p>
+
+					<p class="form-row form-row-first">
+						<input type="text" name="coupon_code" class="form-control" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" id="coupon_code" value="" />
+					</p>
+
+					<p class="form-row form-row-last">
+						<button type="submit" class="btn btn-outline-primary" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply coupon', 'woocommerce' ); ?></button>
+					</p>
+
+					<div class="clear"></div>
+				</form>
 			</div>
 
 			<div class="col-12 col-sm-5">
@@ -49,19 +69,24 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 	<?php endif; ?>
 
-	<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
+	
+	<div id="order_review" class="woocommerce-checkout-review-order">
+		<?php //do_action( 'woocommerce_checkout_order_review' ); ?>
+	</div>
+
+	<?php //do_action( 'woocommerce_checkout_after_order_review' ); ?>
+	
+		
+</form>
+<div>
+<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
 
 	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
 		tttttttttttttttttttttttttt
 	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 eeeeeeeeeeeeeeeee
-	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-	</div>
+</div>
 
-	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
-
-</form>
 
 <?php
 do_action( 'woocommerce_after_checkout_form', $checkout );
